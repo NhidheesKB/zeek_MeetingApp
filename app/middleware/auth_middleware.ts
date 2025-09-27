8da277838f5de47bccaa2217f9e5f84fadcd5ccf
NhidheesKB <nhidhees@gmail.com>
2025-09-27 11:22:18 +0530

    Add log in middleware

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -19,7 +19,7 @@ export default class AuthMiddleware {
       guards?: (keyof Authenticators)[]
     } = {}
   ) {
-
+    console.log(ctx.auth.user)
     await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo })
     return next()
   }
