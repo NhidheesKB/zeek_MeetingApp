61e68145163765442e6b746faf27f2f7fce4d072
NhidheesKB <nhidhees@gmail.com>
2025-10-06 11:20:33 +0530

    change docker image

--- app/services/report_service.ts
+++ app/services/report_service.ts
@@ -1,21 +1,25 @@
+import env from '#start/env';
 import puppeteer from 'puppeteer'
 import view from 'edge.js'
 import Meeting from '#models/meeting'
 import Organization from '#models/organization'
 export class PuppeteerService {
-  async pageContent(meetingDetails: Meeting[], organization: Organization[], summary: string|undefined) {
+  async pageContent(meetingDetails: Meeting[], organization: Organization[], summary: string | undefined) {
     const pageContent = await view.render('report', {
       organizationName: organization[0].name,
       meetingTitle: meetingDetails[0].title,
       meetingDate: meetingDetails[0].date.toISODate(),
       meetingTime: meetingDetails[0].time,
-      participants: meetingDetails[0].participants.split(',')||meetingDetails[0].participants,
+      participants: meetingDetails[0].participants.split(',') || meetingDetails[0].participants,
       summary: summary,
     })
     return pageContent
   }
-  async generateReport(meetingDetails: Meeting[], organization: Organization[], summary: string|undefined) {
-    const browser = await puppeteer.launch()
+  async generateReport(meetingDetails: Meeting[], organization: Organization[], summary: string | undefined) {
+    const browser = await puppeteer.launch({
+      executablePath: env.get(' PUPPETEER_EXECUTABLE_PATH'),
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
     const page = await browser.newPage()
     const pageContent = await this.pageContent(meetingDetails, organization, summary)
     await page.setContent(pageContent, { waitUntil: 'networkidle0' })
